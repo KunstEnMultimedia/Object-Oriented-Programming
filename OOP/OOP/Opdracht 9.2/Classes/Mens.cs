7f1acc3a46678c32b1dc8457757f2a37bc3c97e6
Marinus <mwwretra@st.noorderpoort.nl>
2023-10-02 09:55:56 +0200

    Opdracht 9.2

---
+++ OOP/OOP/Opdracht 9.2/Classes/Mens.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Mens
+{
+	private int geboortjeJaar = 1970;
+	private string lief = "niemand";
+
+
+	public void VeranderLief(string nieuwLief)
+	{
+		lief = nieuwLief;
+	}
+	 public void Praat()
+	 {
+		Console.WriteLine("Ik ben een mens!");
+		//Console.WriteLine($"Ik ben Geboren in {geboortjeJaar}.");
+		VertelGeheim();
+     }
+
+	public void VertelGeheim()
+	{
+		if (lief != "niemand")
+			Console.WriteLine($"Ik ben verliefd op {lief}");
+		else
+			Console.WriteLine("Ik ben op niemand verliefd");
+	   Console.WriteLine("Ik ben verliefd op Anneke");
+	}
+
+	public void StartVerjongingskuur()
+	{
+		Console.WriteLine("Jeuj. Ik word jonger!");
+		geboortjeJaar++;
+	}
+}
---
+++ OOP/OOP/program.cs
@@ -0,0 +1,60 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Opdracht92();
+            Console.ReadLine();
+        }
+          
+        
+        static void Opdracht91() 
+          { 
+              int aantalBalletjes = 1000;
+              Console.CursorVisible = false;
+              Random r = new Random();
+              Balletje[] veelBalletjes = new Balletje[aantalBalletjes];
+             
+              for (int i = 0; i < veelBalletjes.Length; i++)
+              {
+                  veelBalletjes[i] = new Balletje();
+                  veelBalletjes[i].X = r.Next(1, 30);
+                  veelBalletjes[i].Y = r.Next(1,30);
+                  veelBalletjes[i].VectorX = r.Next(-2, 10); 
+                  veelBalletjes[i].VectorY =   r.Next(-2, 10);
+              }
+             
+              while(true) 
+              {
+                  for (int i = 0; i < veelBalletjes.Length; i++)
+                  { 
+                    veelBalletjes[i].Update();
+                  }
+             
+                  for (int i = 0; i < veelBalletjes.Length; i++)
+                  {
+                    veelBalletjes[i].TekenOpScherm();
+                  }
+                  System.Threading.Thread.Sleep(5);
+                  Console.Clear();
+              }   
+          }
+
+        public static void Opdracht92()
+        {
+           Mens deelnemer1 = new Mens();
+           Mens deelnemer2 = new Mens();
+           deelnemer1.Praat();
+           deelnemer2.Praat();
+
+            deelnemer2.VeranderLief("phoebe");
+            deelnemer1.Praat();
+            deelnemer2.Praat();
+
+            deelnemer1.VeranderLief("camilla");
+            deelnemer1.Praat();
+            deelnemer2.Praat();
+        }
+    }
+}
