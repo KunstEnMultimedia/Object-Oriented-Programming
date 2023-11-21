adf43b932d5f88a598c59def3486d6c81b93b53b
Marinus <mwwretra@st.noorderpoort.nl>
2023-11-21 18:26:15 +0100

    Opdracht 10.1

---
+++ OOP/Opdracht_10/Classes/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Student
+{
+    public string Naam { get; set; }
+
+    public Student(string name) 
+    //name is de input property voor de constructor
+    { 
+      Naam = name; // autoproperty wordt name
+    }
+}
---
+++ OOP/Opdracht_10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Opdracht_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Opdracht101();
+            Console.ReadLine();
+        }
+        public static void Opdracht101()
+        {
+            int getal = 5;
+            VerhoogParameter(getal);
+            void VerhoogParameter(int a)
+            {
+                a++;
+                Console.WriteLine($"In methode {a}");
+            }
+            Console.WriteLine($"Na methode {getal}");
+            Console.Write("Naam: ");
+            Student stud = new Student(Console.ReadLine()); // readline defineerd de string name
+            Student stud1 = new Student("Egbert");
+            Console.WriteLine($"stud {stud.Naam}");
+            Console.WriteLine($"stud1 {stud1.Naam}");
+            stud1 = stud;
+            Console.WriteLine($"stud  {stud.Naam}");
+            Console.WriteLine($"stud1 {stud1.Naam}");
+
+
+        }
+    }
+}
