b4bcd8623f1cf456d4fa80afc8a7ca6624be0b51
Marinus <mwwretra@st.noorderpoort.nl>
2024-03-18 07:52:52 +0100

    Opdracht 12.3

--- OOP/Opdracht_12/Program.cs
+++ OOP/Opdracht_12/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Opdracht122();
+            Opdracht123();
             Console.ReadLine();
         }
 
@@ -66,27 +66,50 @@
             listOfStringarrays.Add(["Yo"]);
 
 
-            for (int i = 0; i <= listOfStringarrays.Count-1; i++)
+            for (int i = 0; i <= listOfStringarrays.Count - 1; i++)
             {
                 Console.WriteLine(listOfStringarrays[i].ElementAt(0));
             }
 
-            for (int i = 0; i <= alleGetallen.Count-1; i++)
+            for (int i = 0; i <= alleGetallen.Count - 1; i++)
             {
                 Console.WriteLine(alleGetallen[i]);
             }
 
-            for (int i = 0; i <= presentieLijst.Count-1; i++)
+            for (int i = 0; i <= presentieLijst.Count - 1; i++)
             {
                 Console.WriteLine($"{presentieLijst[i].Naam} is geboren in {presentieLijst[i].GeboorteJaar}");
             }
 
-            for (int i = 0; i <= binaryList.Count-1; i++)
+            for (int i = 0; i <= binaryList.Count - 1; i++)
             {
                 Console.WriteLine(binaryList[i]);
             }
 
             // ik hed de List.clear methode getest het verwijderd alles in een list
+        }
+
+        public static void Opdracht123()
+        {
+            List<Student> presentieLijst = new List<Student>()
+            {
+               new Student() { Naam = "Brent", GeboorteJaar = 1991, IsWerkStudent = false },
+               new Student() { Naam = "Albert", GeboorteJaar = 1989, IsWerkStudent = false },
+               new Student() { Naam = "Marinus", GeboorteJaar = 2004, IsWerkStudent = false},
+               new Student() { Naam = "Jurgen", GeboorteJaar = 2006, IsWerkStudent = true}
+            };
+            foreach (Student student in presentieLijst)
+            {
+                Console.Write($"{student.Naam} is geboren in {student.GeboorteJaar} en is ");
+                Console.WriteLine(student.IsWerkStudent ? "geen werk student" : "wel een werk student");
+            }
+
+            foreach (var student in presentieLijst)
+            {
+                student.GeboorteJaar++;
+                Console.Write($"{student.Naam} is geboren in {student.GeboorteJaar} en is ");
+                Console.WriteLine(student.IsWerkStudent ? "geen werk student" : "wel een werk student");
+            }
         }
     }
 }
