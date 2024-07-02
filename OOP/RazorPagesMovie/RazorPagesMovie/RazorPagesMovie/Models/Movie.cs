a7d4024b2c3b115acb093b12a03d0e1c504ffc43
Marinus Retra <marinus@flowonline.nl>
2024-07-02 14:32:21 +0200

    Opdracht 9.2

---
+++ OOP/RazorPagesMovie/RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; } // Id property is nodig als primary key?
+        public string? Title { get; set; } // ? is to make it nullable
+        [DataType(DataType.Date)] // specificeerd het type data in de ReleaseDate property
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+
+
+    }
+}
